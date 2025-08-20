5886709319c0a4b3a68578cf6442d92c4f7bd291
andrewDzokwom <andrewa441@gmail.com>
2025-08-20 18:03:23 +0100

    Implement input handling for calculator number buttons

---
+++ main.js
@@ -0,0 +1,13 @@
+const inputValue = document.getElementById('user-input')
+
+const number = document.querySelectorAll(".numbers").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    if (inputValue.innerText === "NaN") {
+      inputValue.innerText = "";
+    }
+    if (inputValue.innerText === "0") {
+      inputValue.innerText = "";
+    }
+    inputValue.innerText += e.target.innerHTML.trim();
+  });
+});
