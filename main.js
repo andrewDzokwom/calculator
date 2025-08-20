0fe2e8ce4a4454f823956149948cf9c37b7a6ba9
andrewDzokwom <andrewa441@gmail.com>
2025-08-20 18:15:05 +0100

    Refactor calculation logic and enhance input handling for operations and handle decimal to not exceed 3 dps

--- main.js
+++ main.js
@@ -17,6 +17,25 @@ const calculate = document
   .forEach(function (item) {
     item.addEventListener("click", function (e) {
       console.log(e.target.innerHTML);
-      
+      let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
+
+      if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+        let result = eval(inputValue.innerText);
+        if (typeof result === "number" && !Number.isInteger(result)) {
+            result = Number(result.toFixed(3));
+        }
+        inputValue.innerText = result;
+      } else if (e.target.innerHTML === "AC") {
+          inputValue.innerText = 0;
+      } else if (e.target.innerHTML === "DEL") {
+          inputValue.innerText = inputValue.innerText.substring(0,inputValue.innerText.length - 1);
+          if (inputValue.innerText.length == 0) {
+              inputValue.innerText = 0;
+          }
+      } else {
+          if (!isNaN(lastValue)) {
+              inputValue.innerText += e.target.innerHTML;
+          }
+      }
     });
   });
